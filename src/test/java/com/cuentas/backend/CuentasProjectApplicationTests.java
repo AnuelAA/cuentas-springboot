17fdee3b8f70a646781133f49385e19073d8e044
Jose Nieto Lopez <jnietolo@mercadona.es>
2025-09-17 11:23:55 +0200

    init Project

---
+++ src/test/java/com/cuentas/backend/CuentasProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuentas.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuentasProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
